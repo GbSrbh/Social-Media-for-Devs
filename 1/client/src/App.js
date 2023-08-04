bdbf87e4defd80b180a75bd2fde05ea8a4d40bac
Srbh0205 <saurabhbhrambhatt@jklu.edu.in>
2023-08-04 15:00:56 +0530

    CreateProfile Form

--- 1/client/src/App.js
+++ 1/client/src/App.js
@@ -5,6 +5,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from './components/layout/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -29,6 +30,7 @@ function App() {
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/dashboard" element={<PrivateRoute> <Dashboard /> </PrivateRoute>} />
+        <Route exact path='/create-profile' element={<CreateProfile />} />
       </Routes>
     </section>
   );
